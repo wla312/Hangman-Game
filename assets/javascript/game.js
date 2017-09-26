a12caa10647c010754ad492b0a69322d3b438595
William Alt <williamalt@WLAs-MacBook-Air.local>
2017-09-26 18:09:28 -0500

    updates to match solution

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,158 @@
 // Hangman JS
 
-// Choose a theme
+// GLOBAL VARIABLES FIRST
 
-// Theme = Summer Olympics
+// Choose a theme, Theme = Summer Olympics
 
 // Within that theme, create an array of possible hangman words
-var hangmanWords = ["swimming", "diving", "gymnastics"]
-// Set the scoreboard
-var wins = 0;
-var guessesLeft = 15;
+var wordsList = ["swimming", "diving", "gymnastics"];
 
-// Have the computer randomly select on of the words from the array
-var computerWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
+// computer selected solution will be held here
+var chosenWord = "";
 
-// console.log(computerWord);
+// store individual letters of the solution in an array
+var lettersInChosenWord = [];
 
-// Have the computer display the number of letters the word has
-var wordLength = computerWord.length
+// based on the solution, show a specified number of blanks
+var numBlanks = 0;
 
-// console.log(wordLength);
+// create a variable to hold the mix of blanks and solved letters
+var blanksAndSuccesses = [];
 
-var wordDisplay = [];
-	for (i=0; i<wordLength; i++) {
-		wordDisplay.push(" _ ");
+// create a variable to hold all the wrong guesses
+var wrongGuesses = [];
+
+// create a variable to hold all the letters guessed
+var letterGuessed = [];
+
+// game counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
+
+// ==========================================================================
+// startGame() will start and restart the game
+
+function startGame() {
+
+	// reset guesses back to zero
+	numGuesses = 9;
+
+	// solution chosen randomly from wordList
+	chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
+
+	// break the chosen word into individual letters
+	lettersInChosenWord = chosenWord.split("");
+
+	// count the number of letters in the word
+	numBlanks = lettersInChosenWord.length;
+
+	// test
+	console.log(chosenWord);
+
+	// reset the guess and success array
+	blanksAndSuccesses = [];
+
+	// reset the wrong guesses from the previous round
+	wrongGuesses = [];
+
+	// put the correct number of blanks on the page
+	for (var i = 0; i<numBlanks; i++) {
+		blanksAndSuccesses.push("_");
 	}
 
-console.log(wordDisplay);
+	// test
+	console.log(blanksAndSuccesses);
 
-// separate the computer selected word into an array of all of its' characters
-var computerWordSplit = Array.from(computerWord);
-console.log(computerWordSplit);
+	// reprint GuessesLeft back to 9
+	document.getElementById("guesses-left").innerHTML = numGuesses;
 
-// create variable to store user guessed letters
-var guessedLetters = [];
+	// print the blanks at the beginning of each round
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
 
-// start game on user keyup
-document.onkeyup = function(event) {
-	// user guesses a letter
-	var userGuess = event.key;
-	// guessed letter is entered into an array of user guessed letters
-    guessedLetters.push(userGuess);
-	
-	console.log(userGuess);
-	// console.log(guessedLetters);
+	// clear the wrong guesses from the previous round
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
-// if guessed letter = letter in the computer word, display that letter
-	if (userGuess === computerWordSplit[0]) {
-		wordDisplay.splice([0], 1, userGuess);
-		
-	}  
-	console.log(wordDisplay);
-
-
-		/*else if (userGuess !== computerWordSplit[i]) {
-		// number of remaining guesses decrements
-		guessesLeft = guessesLeft -1;
-		// if guesses run out before guessing the word, lose
-	}  
-
-		if (guesses === 0) {
-		// computer picks a new word
-		var computerWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
-		// reset guesses to fifteen
-		guessesLeft = 15;
-		// reset the guessed letters
-		guessedLetters = [];
-	}*/
-
-
-// dynamic updates to innerHMTL
-var html = 
-	"<p>Wins: " + "</p>" +
-	"<p>Current Word: " + wordDisplay + "</p>" +
-	"<p>Number of Guesses Remaining: " + guessesLeft + "</p>" +
-	"<p>Letters Already Guessed: " + guessedLetters + "</p>";
-
-	document.querySelector("#game").innerHTML = html;
 }
+
+// checkLetters() function will do comparisons for matches
+
+function checkLetters(letter) {
+
+	// boolean based on whether or not a letter is found anywhere in the word
+	var letterInWord = false;
+
+	// check if a letter exists inside the array at all
+	for (var i = 0; i < numBlanks; i++) {
+		if (chosenWord[i] === letter) {
+			letterInWord = true;
+		}
+	}
+	// if the letter is in the word, find where exactly it is
+	if (letterInWord) {
+		for (var j = 0; j < numBlanks; j++) {
+			if (chosenWord[j] === letter) {
+				blanksAndSuccesses[j] = letter;
+			}
+		}
+		// test
+		console.log(blanksAndSuccesses);
+	}
+	else {
+		// add letter to the list of wrong letters
+		wrongGuesses.push(letter);
+		// subtract a guess
+		numGuesses--;
+	}
+}
+
+// roundComplete() function will run after each guess is made
+
+function roundComplete() {
+
+	// log initial status update in console
+	console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
+
+	// update HTML
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+	// if hangman string equals solution, win
+	if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+		winCounter ++;
+		alert("you win!");
+		document.getElementById("win-counter").innerHTML = winCounter;
+		startGame();
+	}
+	else if (numGuesses === 0) {
+		lossCounter++;
+		alert("you lose!")
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+		startGame();
+	}
+}
+
+// =====================================================================
+
+startGame();
+
+// initiate the function for capturing key clicks
+document.onkeyup = function(event) {
+	// convert all key clicks into lowercase letters
+	letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+
+	// call the function to check for any correct guesses
+	checkLetters(letterGuessed);
+
+	// call the function that ends each round
+	roundComplete();
+};
+
+
+
+
+
+
+
+
